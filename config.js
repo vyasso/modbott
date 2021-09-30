f430491b79547b6cccbbefbda000fb2064370ff3
vyasso <89705636+vyasso@users.noreply.github.com>
2021-09-30 20:23:42 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,11 +3,11 @@ module.exports = {
  "description": "",
  "domain": "", // This Domain need to be without https:// or http:// (eg: hydrabot.fun not https://hydrabot.fun)
  "google_analitics": "", 
- "token": "",
+ "token": "ODkzMjAwNTIwNTE5NDk5ODE2.YVX_zg.TpOU9wCtFEol8VIiEumGPSIE7K4",
  "https":"https://",
  "port":"5003",
- "client_id":"",
- "secret":"",
- "support_server_link":"",
- "bot_name":""
+ "client_id":"893200520519499816",
+ "secret":"tRa7hYyjnnRozZ_JUu2h2H44wMTo65iP",
+ "support_server_link":"https://discord.gg/ChTax9vY3h",
+ "bot_name":"AL3X"
 }
